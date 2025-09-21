219c2192910e41413e052b5bbbf18d0a91a856f7
Andrew Speer <andrew.speer@isolutions.com.au>
2025-09-21 19:48:55 +0930

    Try new WEBDYNE_HEAD_INSERT method of style

--- docker/webdyne.conf.pl
+++ docker/webdyne.conf.pl
@@ -3,10 +3,11 @@ $_={
     'WebDyne::Constant' => {
         WEBDYNE_ERROR_SHOW          => 1,
         WEBDYNE_ERROR_SHOW_EXTENDED => 1,
-        WEBDYNE_HTML_PARAM          => {
-            %{ $WebDyne::Constant::WEBDYNE_HTML_PARAM },
-            style => 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css'
-        }
+        #WEBDYNE_HTML_PARAM0          => {
+        #    %{ $WebDyne::Constant::WEBDYNE_HTML_PARAM },
+        #    style => 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css'
+        #},
+        WEBDYNE_HEAD_INSERT         => '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"><style>:root { --pico-font-size: 85% } body { margin-top: 10px; margin-left: 10px; }</style>',
     }
 }
 
